906d914e07e3ec6d0631edb61f8ab46e6196cdf8
Vladimir Ivakhno <wvovaw0@gmail.com>
2024-07-30 18:19:10 +0500

    pass stage 1

--- app/main.ts
+++ app/main.ts
@@ -1,10 +1,14 @@
 import * as net from "net";
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-console.log("Logs from your program will appear here!");
-
-// Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
+  socket.on("data", (buffer) => {
+    const decoder = new TextDecoder();
+    const data = decoder.decode(buffer);
+    console.log("Data recieved: ", data);
+
+    socket.write(Buffer.from("HTTP/1.1 200 OK\r\n\r\n"));
+  });
+
   socket.on("close", () => {
     socket.end();
   });
